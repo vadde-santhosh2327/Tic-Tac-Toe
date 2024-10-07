d972a7df3214a96f5595618a14af6eb071116196
unknown <vadde.santhosh50@gmail.com>
2024-10-07 10:36:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 
 import React, { Component } from 'react'
 import Tictac from './Components/Tictac'
-import Facebook from './Components/Facebook'
-import Weather from './Components/Weather'
 export default class App extends Component {
   render() {
     return (
       <div>
-        {/* <Tictac/> */}
-        {/* <Facebook/> */}
-        <Weather/>
+        <Tictac/>
+       
       </div>
     )
   }
